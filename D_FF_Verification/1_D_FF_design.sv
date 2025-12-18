19b77758a86c5f9f519f5518c3a502d9b4a3919e
Harish <harishmurugan8503@gmail.com>
2025-12-18 22:30:50 +0530

    D_FF verification environment

---
+++ D_FF_Verification/1_D_FF_design.sv
@@ -0,0 +1,13 @@
+
+module d_ff (input  logic clk,rst,d,output logic q_out);
+  
+  always_ff @(posedge clk or posedge rst)
+    begin
+      if (rst)
+        q_out<=1'b0;
+      else 
+        q_out<=d;
+    end
+  
+endmodule
+        
