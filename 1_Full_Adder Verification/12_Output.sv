2fb7a50bff2bf2a2fd50e43aed10a6edaa7a5f2b
Harish <harishmurugan8503@gmail.com>
2026-01-30 23:49:08 +0530

    Add output verification for full adder simulation

---
+++ 1_Full_Adder Verification/12_Output.sv
@@ -0,0 +1,85 @@
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Jan 30 13:18 2026
+generator class signal
+a=1,b=1,c=1,sum=0,carry=0
+driver signal
+a=1,b=1,c=1,sum=0,carry=0
+monitor class signals
+a=1,b=1,c=1,sum=1,carry=1
+signals received on score board
+a=1,b=1,c=1,sum=1,carry=1
+--------pass-------
+Coverage = 40.62 %
+generator class signal
+a=1,b=1,c=0,sum=0,carry=0
+driver signal
+a=1,b=1,c=0,sum=0,carry=0
+monitor class signals
+a=1,b=1,c=0,sum=0,carry=1
+signals received on score board
+a=1,b=1,c=0,sum=0,carry=1
+--------pass-------
+Coverage = 56.25 %
+generator class signal
+a=1,b=0,c=1,sum=0,carry=0
+driver signal
+a=1,b=0,c=1,sum=0,carry=0
+monitor class signals
+a=1,b=0,c=1,sum=0,carry=1
+signals received on score board
+a=1,b=0,c=1,sum=0,carry=1
+--------pass-------
+Coverage = 71.88 %
+generator class signal
+a=1,b=0,c=0,sum=0,carry=0
+driver signal
+a=1,b=0,c=0,sum=0,carry=0
+monitor class signals
+a=1,b=0,c=0,sum=1,carry=0
+signals received on score board
+a=1,b=0,c=0,sum=1,carry=0
+--------pass-------
+Coverage = 75.00 %
+generator class signal
+a=0,b=1,c=1,sum=0,carry=0
+driver signal
+a=0,b=1,c=1,sum=0,carry=0
+monitor class signals
+a=0,b=1,c=1,sum=0,carry=1
+signals received on score board
+a=0,b=1,c=1,sum=0,carry=1
+--------pass-------
+Coverage = 90.62 %
+generator class signal
+a=0,b=1,c=0,sum=0,carry=0
+driver signal
+a=0,b=1,c=0,sum=0,carry=0
+monitor class signals
+a=0,b=1,c=0,sum=1,carry=0
+signals received on score board
+a=0,b=1,c=0,sum=1,carry=0
+--------pass-------
+Coverage = 93.75 %
+generator class signal
+a=0,b=0,c=1,sum=0,carry=0
+driver signal
+a=0,b=0,c=1,sum=0,carry=0
+monitor class signals
+a=0,b=0,c=1,sum=1,carry=0
+signals received on score board
+a=0,b=0,c=1,sum=1,carry=0
+--------pass-------
+Coverage = 96.88 %
+generator class signal
+a=0,b=0,c=0,sum=0,carry=0
+driver signal
+a=0,b=0,c=0,sum=0,carry=0
+monitor class signals
+a=0,b=0,c=0,sum=0,carry=0
+signals received on score board
+a=0,b=0,c=0,sum=0,carry=0
+--------pass-------
+Coverage = 100.00 %
+<<-------100% coverbins hitted------->>
+$finish called from file "environment.sv", line 62.
+$finish at simulation time                   36
