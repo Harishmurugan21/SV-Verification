20d1197a8420f52a5a11c9835090a0a68bd1bc09
Harish <harishmurugan8503@gmail.com>
2026-03-11 23:57:34 +0530

    Testbench - topmodule
    Updated the depth parameter from 8 to 16 in the testbench and cleaned up the code formatting.

--- 4_Sync_FIFO_Verification/03_tb_Topmodule.sv
+++ 4_Sync_FIFO_Verification/03_tb_Topmodule.sv
@@ -1,47 +1,35 @@
-//testbench
 `include "interface.sv"
 `include "test.sv"
+
 module tb;
-  
   parameter W=8;
-  parameter D=8;
-  
+  parameter D=16;
   
   //create handle for interface
   intf #(.width(W), .depth(D)) intf_tb();
   //pass the interface handle to test environment
   test #(W,D) name;
-  
+
   initial begin
     name=new(intf_tb);
     name.test();
   end
-  
-  
-  //dut 
-  
+
+  //instansiate to DUT
   sync_fifo  #(W,D) dut (.clk(intf_tb.clk),
-                        .rst(intf_tb.rst),
-                        .w_en(intf_tb.w_en),
-                        .r_en(intf_tb.r_en),
-                        .data_in(intf_tb.data_in),
-                        .data_out(intf_tb.data_out),
-                        .full(intf_tb.full),
-                        .empty(intf_tb.empty));
-  
+                         .rst(intf_tb.rst),
+                         .w_en(intf_tb.w_en),
+                         .r_en(intf_tb.r_en),
+                         .data_in(intf_tb.data_in),
+                         .data_out(intf_tb.data_out),
+                         .full(intf_tb.full),
+                         .empty(intf_tb.empty));
+
   initial begin
     intf_tb.clk=0;
     forever #5 intf_tb.clk=~intf_tb.clk;
   end
   
-  
-  
-    
-  //end sumulation time
-  initial #600 $finish;
-
-  
 endmodule
-  
-  
-  
+
+
